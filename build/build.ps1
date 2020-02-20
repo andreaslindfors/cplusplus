89ffd7b36f085128a3b48222b80037fc0936f9e7
Andreas Lindfors <andreas.lindfors@aditro.com>
2020-02-20 22:44:24 +0100

    build script

---
+++ build/build.ps1
@@ -0,0 +1,11 @@
+## x86_64-w64-mingw32-gcc .\src\HelloWorld1.cpp
+## x86_64-w64-mingw32-gcc .\src\HelloWorld1.cpp
+## x86_64-w64-mingw32-gcc .\src\HelloWorld1.cpp
+## x86_64-w64-mingw32-gcc .\src\HelloWorld1.cpp
+x86_64-w64-mingw32-gcc .\src\HelloWorld1.cpp
+x86_64-w64-mingw32-gcc .\src\HelloWorld2.cpp
+x86_64-w64-mingw32-gcc .\src\Concurrent1.cpp
+x86_64-w64-mingw32-gcc .\src\Concurrent2.cpp
+
+
+
